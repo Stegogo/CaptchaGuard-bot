5bb72f713b12fd04d9165ec2f49ecd2ce3290d81
stegogo <lizmahrk@gmail.com>
2021-07-15 21:35:08 +0300

    1.9: Improvements for Heroku deploy

--- middlewares/language_middlware.py
+++ middlewares/language_middlware.py
@@ -2,12 +2,12 @@ from typing import Tuple, Any
 from aiogram import types
 from aiogram.contrib.middlewares.i18n import I18nMiddleware
 
-import main
+import app
 from data.config import I18N_DOMAIN, LOCALES_DIR
 
 class ACLMiddleware(I18nMiddleware):
     async def get_user_locale(self, action: str, args: Tuple[Any]):
-        return await main.get_lang()
+        return await app.get_lang()
 
 def setup_middleware(dp):
     i18n = ACLMiddleware(I18N_DOMAIN, LOCALES_DIR)
