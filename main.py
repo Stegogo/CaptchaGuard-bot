7fc607cb65d5bba4d5490e52906f69882bbefab5
stegogo <lizmahrk@gmail.com>
2021-07-16 11:24:08 +0300

    1.9: Improvements for Heroku deploy

--- main.py
+++ main.py
@@ -5,25 +5,19 @@ from loader import bot
 import os
 import psycopg2
 
+os.environ[
+    'DATABASE_URL'] = "postgres://jyufqecsbuaufz:ba2658f4cb8d34361fe71d8d7e4ddea9f711a909abda2f985046d3e1770fd177@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/dbfno3t6nc4qq6"
 DATABASE_URL = os.environ['DATABASE_URL']
-
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-
-import os
-import psycopg2
-
-
-os.environ['DATABASE_URL'] = "postgres://jyufqecsbuaufz:ba2658f4cb8d34361fe71d8d7e4ddea9f711a909abda2f985046d3e1770fd177@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/dbfno3t6nc4qq6"
-DATABASE_URL = os.environ['DATABASE_URL']
-
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 async def on_startup(dispatcher):
     await on_startup_notify(dispatcher)
 
+
 async def on_shutdown(dispatcher):
     await bot.close()
+
 
 async def get_lang():
     from commhandlers import database
@@ -38,6 +32,8 @@ async def get_lang():
         else:
             return 'en'
 
+
 if __name__ == '__main__':
     from commhandlers import dp
+
     executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
