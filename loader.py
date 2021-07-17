197ca5f242caf721979dd3330224c855fd8497f2
stegogo <lizmahrk@gmail.com>
2021-07-17 20:23:52 +0300

    1.9: Improvements for Heroku deploy

--- loader.py
+++ loader.py
@@ -10,9 +10,9 @@ from sql import create_pool
 import os
 import psycopg2
 
-os.environ['DATABASE_URL'] = "postgres://mgfxmdcettahps:cd1a197a7e128b11e3f86e6aa0de5a87ae45494fca839837cbedd9bbb1385c9a@ec2-54-220-195-236.eu-west-1.compute.amazonaws.com:5432/darcoeb8hro3p1"
+os.environ['DATABASE_URL'] = "postgres://jyufqecsbuaufz:ba2658f4cb8d34361fe71d8d7e4ddea9f711a909abda2f985046d3e1770fd177@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/dbfno3t6nc4qq6"
 DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+conn = psycopg2.connect(DATABASE_URL, sslmode='require', port=process.env.PORT or 8080)
 
 logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
                     level=logging.INFO)
