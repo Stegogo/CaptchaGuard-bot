d748e57277aa41743cb113aae13ccf6c2175725e
stegogo <lizmahrk@gmail.com>
2021-07-17 20:50:19 +0300

    1.9

---
+++ app.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    from commhandlers import dp
+
+    executor.start_polling(dp, on_startup=main.on_startup, on_shutdown=main.on_shutdown)
--- main.py
+++ main.py
@@ -5,15 +5,6 @@ from loader import bot
 import os
 import psycopg2
 
-#POSTGRESURI = os.environ['POSTGRESURI']
-#db = Gino()
-
-#async def create_db():
-
-    #await db.set_bind(POSTGRESURI)
-    #db.gino: GinoSchemaVisitor
-    #await db.gino.create_all()
-
 async def on_startup(dispatcher):
     await on_startup_notify(dispatcher)
 
@@ -34,9 +25,3 @@ async def get_lang():
             return types.User.get_current().language_code
         else:
             return 'en'
-
-
-if __name__ == '__main__':
-    from commhandlers import dp
-
-    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
