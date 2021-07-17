1f0d5dbdbb0dde0e43bf3d33271235664fbb933a
stegogo <lizmahrk@gmail.com>
2021-07-17 18:26:10 +0300

    1.9: Improvements for Heroku deploy

--- loader.py
+++ loader.py
@@ -7,6 +7,12 @@ import logging
 import gettext
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from sql import create_pool
+import os
+import psycopg2
+
+os.environ['DATABASE_URL'] = "postgres://jyufqecsbuaufz:ba2658f4cb8d34361fe71d8d7e4ddea9f711a909abda2f985046d3e1770fd177@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/dbfno3t6nc4qq6"
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
                     level=logging.INFO)
@@ -18,4 +24,4 @@ dp = Dispatcher(bot, storage=storage)
 from middlewares.language_middlware import setup_middleware
 i18n = setup_middleware(dp)
 _ = i18n.gettext
-db = loop.run_until_complete(create_pool())
+db = conn
