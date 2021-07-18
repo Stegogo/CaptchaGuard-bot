2a9ba2408cffd59256e8d5bbce8853f0b1492895
stegogo <lizmahrk@gmail.com>
2021-07-18 05:48:06 +0300

    1.9

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from aiogram import executor
+from utils.notify_admins import on_startup_notify, on_shutdown_notify
 
 if __name__ == '__main__':
     from commhandlers import dp
 
-    executor.start_polling(dp, on_startup=main.on_startup, on_shutdown=main.on_shutdown)
+    executor.start_polling(dp, on_startup=on_startup_notify, on_shutdown=on_shutdown_notify)
