b27427b2080491306f1885942d895728209aafb4
stegogo <lizmahrk@gmail.com>
2021-07-18 18:52:56 +0300

    2.0 Rewrote db interactions for psycopg2

--- loader.py
+++ loader.py
@@ -13,26 +13,6 @@ import psycopg2
 import urllib.parse as urlparse
 import os
 
-url = urlparse.urlparse(os.getenv('DATABASE_URL'))
-db_url = os.environ.get('DATABASE_URL')
-dbname = os.environ.get('DB_DATABASE')
-user = os.environ.get('DB_USER')
-password = os.environ.get('DB_PASSWORD')
-host = os.environ.get('DB_HOST')
-port = os.environ.get('DB_PORT')
-print(os.environ.get('DATABASE_URL'))
-print(os.environ.get('DB_PORT'))
-
-
-conn = psycopg2.connect(
-            db_url,
-            user=user,
-            password=password,
-            host=host,
-            port=port,
-            sslmode='require'
-            )
-
 url1 = urlparse.urlparse(os.environ['DATABASE_URL'])
 dbname1 = url1.path[1:]
 user1 = url1.username
@@ -47,10 +27,7 @@ con = psycopg2.connect(
             host=host1,
             port=port1
             )
-
-#DATABASE_URL = "postgres://jyufqecsbuaufz:ba2658f4cb8d34361fe71d8d7e4ddea9f711a909abda2f985046d3e1770fd177@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/dbfno3t6nc4qq6"
-#DATABASE_URL = os.getenv('DATABASE_URL')
-#conn = psycopg2.connect(DATABASE_URL, sslmode='require', port=os.getenv('PORT'))
+con.autocommit = True
 
 logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
                     level=logging.INFO)
